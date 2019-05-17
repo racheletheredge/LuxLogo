ad954aa4ae720e0aed94ef9c8f94474521c92b6e
david lobser <davidlobser@yahoo.com>
2019-05-17 12:06:37 -0400

    made logo

---
+++ Assets/Materials/wavy.shader
@@ -0,0 +1,96 @@
+﻿Shader "Unlit/wavy"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			#define PI 3.14159265358
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+
+
+float normY(float pixel) {
+    return pixel / 100;
+}
+
+float4 mainImage(in float2 fragCoord )
+{
+	float2 xy = fragCoord.xy / float2(100,1);
+	
+    float4 color = float4(1.0, 1.0, 1.0, 1.0);
+    float buffer = normY(5.0);
+    const int amt = 5;
+    float amtf = float(amt);
+    
+    float sinVal;
+    bool colored = false;
+    
+    for(int i = 0; i < amt; i++) {
+        sinVal = sin(xy.x * 12.0 * PI - _Time.x) / amtf + float(i) / amtf;
+
+        if(xy.y < sinVal + buffer && xy.y > sinVal - buffer) {
+            color.r = xy.y;
+            color.g = xy.x;
+            colored = true;
+            break;
+        }
+    }
+    
+    if (!colored && xy.y < sinVal) {
+            color.r = xy.x;
+            color.g = xy.y;
+    }
+    
+    return color;
+}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return mainImage(i.uv);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/DOScaleTextBlah.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening.Core;
+using DG.Tweening;
+using MirzaBeig.Scripting.Effects;
+
+public class DOScaleTextBlah : MonoBehaviour {
+
+
+	List<Vector3> initPos;
+	public float gapBetweenWords = .5f;
+	public float gapBetweenLetters = .1f;
+	public float scaleUpSpeed = 1;
+	float count;
+	public float noiseSpeed = .4f;
+	public float noiseAmount = .025f;
+	int counter;
+	public Ease ease;
+
+	public bool reset = true;
+
+	public float startTime = 5;
+	float timeCounter = 0;
+
+
+	public Vector3 scaleTo;
+
+	void Start () {
+		Init ();
+	}
+
+
+	Vector3 GetNoisePosition(Vector3 v, Vector3 t){
+		return new Vector3 (
+			Noise2.perlin (t.x+v.x+.01f, t.y+v.y, t.z+v.z),
+			Noise2.perlin (t.z+v.x, t.x+v.y+.01f, t.y+v.z),
+			Noise2.perlin (t.y+v.x, t.z+v.y, t.x+v.z+.01f));
+	}
+
+	void Init(){
+		initPos = new List<Vector3> ();
+
+		for (int i = 0; i < this.transform.childCount; i++) {
+			for (int j = 0; j < this.transform.GetChild(0).childCount; j++) {
+				Transform t = this.transform.GetChild (0).GetChild (j);
+				initPos.Add (t.localPosition);
+				t.localPosition = new Vector3 (t.localPosition.x, t.localPosition.y + 10.15f, t.localPosition.z);
+				t.localScale = Vector3.zero;
+			}
+		}
+	}
+
+	void Animate(){
+		
+		
+		count = 0;
+		int p = 0;
+
+		for (int i = 0; i < this.transform.childCount; i++) {
+			for (int j = 0; j < this.transform.GetChild(0).childCount; j++) {
+				Transform t = this.transform.GetChild (0).GetChild (j);
+				t.DOScale (scaleTo, scaleUpSpeed).SetEase (ease).SetDelay (count);
+				t.DOLocalMove (initPos[p], scaleUpSpeed).SetEase (ease).SetDelay (count);
+			
+				p+=1;
+				count+=gapBetweenLetters;
+			}
+			count += gapBetweenWords;
+		}
+		count = 0;
+	}
+		
+	void Update () {
+		if (reset) {
+			Animate ();
+			reset = false;
+		}
+	}
+}
