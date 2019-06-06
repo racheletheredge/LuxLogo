056b227f52d04d632caf72c56814765941ed667a
david lobser <davidlobser@yahoo.com>
2019-06-06 14:34:44 -0400

    tweaks

---
+++ Assets/Scripts/cmdline.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmdline : MonoBehaviour {
+
+	public string args;
+	public string command;
+	public bool start = false;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (start) {
+			System.Diagnostics.Process.Start (command, args);
+			start = false;
+		}
+	}
+}
